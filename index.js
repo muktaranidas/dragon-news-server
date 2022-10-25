626a4130c6bae6422e0e247f3debf678924a2b1b
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2022-10-25 03:34:37 -0700

    first commit

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = process.envPORT || 5000;
 app.use(cors());
 
 const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
 app.get("/", (req, res) => {
   res.send("News API Running");
@@ -15,6 +16,27 @@ app.get("/", (req, res) => {
 
 app.get("/news-categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/categories/:id", (req, res) => {
+  const id = req.params.id;
+  if (id == "08") {
+    res.send(news);
+  } else {
+    // filter karon eki rokom onk news._id ache
+    const category_news = news.filter((news) => news.category_id === id);
+    res.send(category_news);
+  }
+});
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  // find karon id holo uniqe
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
 });
 
 app.listen(port, () => {
